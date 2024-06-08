ff27247cde0082468c44dcdb4a3bc0cbbd075d0b
SeanAverS <seanasuguitan@gmail.com>
2024-06-08 13:02:09 -0700

    Import components and configure routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import VideoList from './components/VideoList';
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import VideoDetail from "./components/VideoDetail";
+import VideoList from "./components/VideoList";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <VideoList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route path="/" element={<VideoList />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
