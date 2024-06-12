355235e94bca55fd82ce6a513b70a667c6cca866
SeanAverS <seanasuguitan@gmail.com>
2024-06-11 22:56:05 -0700

    Link to home page on logo click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import VideoDetail from "./components/VideoDetail";
 import VideoList from "./components/VideoList";
 import "./App.css";
@@ -9,7 +9,9 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
           <Routes>
             <Route path="/" element={<VideoList />} />
             <Route path="/video/:id" element={<VideoDetail />} />
